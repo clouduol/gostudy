b3da53d6f47d0308fb4392545fec1ae3749c5786
learnMachining <guoyunlong16@mails.ucas.ac.cn>
2017-11-13 15:08:55 +0800

    add type assertion

---
+++ ch7/typeAssert.go
@@ -0,0 +1,41 @@
+// TypeAssert demonstrate interface type assertion
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+type ByteCounter int64
+
+func (b *ByteCounter) Write(p []byte) (n int, err error) { return 0, nil }
+
+func main() {
+	var w io.Writer
+	w = os.Stdout
+	f := w.(*os.File)
+	fmt.Printf("f: %T\n", f)
+
+	rw := w.(io.ReadWriter)
+	fmt.Printf("rw: %T\n", rw)
+
+	w = new(ByteCounter)
+	rw, ok := w.(io.ReadWriter)
+	if ok {
+		fmt.Printf("rw: %T\n", rw)
+	} else {
+		fmt.Printf("rw == nil\n")
+	}
+
+	c, ok := w.(*bytes.Buffer)
+	//fmt.Printf("c: %T\n", w.(*bytes.Buffer))
+	if ok {
+		fmt.Printf("c: %v\n", c)
+	} else {
+		fmt.Printf("c == nil\n")
+	}
+
+	fmt.Println("main end")
+}
