a6271dc7af6dd4c72fa64ae27ba56888d8a4a949
learnMachining <guoyunlong16@mails.ucas.ac.cn>
2018-01-24 10:11:21 +0800

    test import and const

--- misc/multifiles/bar.go
+++ misc/multifiles/bar.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+const foo string = "foo"
+
+//const foobar string = "another foobar"
+
 func bar(info string) {
 	fmt.Println(info)
+	fmt.Println(foobar)
 }
--- misc/multifiles/main.go
+++ misc/multifiles/main.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
+const foobar string = "foobar"
+
 func main() {
 	fmt.Println("--- test multiple files in one package ---")
 	bar("success")
+	fmt.Println(foo)
 }
